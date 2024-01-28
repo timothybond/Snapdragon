a908a34c285a356af8104d849e63b066e5a88302
tim <9168829+timothybond@users.noreply.github.co>
2024-01-28 17:12:53 -0600

    Adding tests for Star-Lord and Gamora.

---
+++ Snapdragon.Tests/SnapCardsTest/GamoraTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Snapdragon.Tests.SnapCardsTest
+{
+    public class GamoraTests
+    {
+        [Test]
+        [TestCase(Side.Top, Column.Middle)]
+        [TestCase(Side.Bottom, Column.Middle)]
+        public void PlayCardAlone_PowerIsSeven(Side side, Column column)
+        {
+            var game = TestHelpers.PlayCards(
+                6,
+                side == Side.Top ? new[] { ("Gamora", column) } : new (string, Column)[] { },
+                side == Side.Bottom ? new[] { ("Gamora", column) } : new (string, Column)[] { }
+            );
+
+            Assert.That(game[column][side].Count == 1);
+
+            var rocketRaccoon = game[column][side][0];
+            Assert.That(rocketRaccoon.Name, Is.EqualTo("Gamora"));
+
+            Assert.That(rocketRaccoon.Power, Is.EqualTo(7));
+        }
+
+        [Test]
+        [TestCase(Side.Top, Column.Middle)]
+        [TestCase(Side.Bottom, Column.Middle)]
+        public void PlayCardWithAnotherCardOnSameSide_PowerIsSeven(Side side, Column column)
+        {
+            var game = TestHelpers.PlayCards(
+                6,
+                side == Side.Top
+                    ? new[] { ("Gamora", column), ("Misty Knight", column) }
+                    : new (string, Column)[] { },
+                side == Side.Bottom
+                    ? new[] { ("Gamora", column), ("Misty Knight", column) }
+                    : new (string, Column)[] { }
+            );
+
+            Assert.That(game[column][side].Count == 2);
+
+            var rocketRaccoon = game[column][side][0];
+            Assert.That(rocketRaccoon.Name, Is.EqualTo("Gamora"));
+
+            Assert.That(rocketRaccoon.Power, Is.EqualTo(7));
+        }
+
+        [Test]
+        [TestCase(Side.Top, Column.Middle)]
+        [TestCase(Side.Bottom, Column.Middle)]
+        public void PlayCardWithAnotherCardOnOpposingSide_PowerIsTwelve(Side side, Column column)
+        {
+            var game = TestHelpers.PlayCards(
+                6,
+                side == Side.Top
+                    ? new[] { ("Gamora", column) }
+                    : new[] { ("Misty Knight", column) },
+                side == Side.Bottom
+                    ? new[] { ("Gamora", column) }
+                    : new[] { ("Misty Knight", column) }
+            );
+
+            Assert.That(game[column][side].Count == 1);
+
+            var rocketRaccoon = game[column][side][0];
+            Assert.That(rocketRaccoon.Name, Is.EqualTo("Gamora"));
+
+            Assert.That(rocketRaccoon.Power, Is.EqualTo(12));
+        }
+    }
+}
---
+++ Snapdragon.Tests/SnapCardsTest/StarLordTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Snapdragon.Tests.SnapCardsTest
+{
+    public class StarLordTests
+    {
+        [Test]
+        [TestCase(Side.Top, Column.Middle)]
+        [TestCase(Side.Bottom, Column.Middle)]
+        public void PlayCardAlone_PowerIsTwo(Side side, Column column)
+        {
+            var game = TestHelpers.PlayCards(
+                3,
+                side == Side.Top ? new[] { ("Star-Lord", column) } : new (string, Column)[] { },
+                side == Side.Bottom ? new[] { ("Star-Lord", column) } : new (string, Column)[] { }
+            );
+
+            Assert.That(game[column][side].Count == 1);
+
+            var rocketRaccoon = game[column][side][0];
+            Assert.That(rocketRaccoon.Name, Is.EqualTo("Star-Lord"));
+
+            Assert.That(rocketRaccoon.Power, Is.EqualTo(2));
+        }
+
+        [Test]
+        [TestCase(Side.Top, Column.Middle)]
+        [TestCase(Side.Bottom, Column.Middle)]
+        public void PlayCardWithAnotherCardOnSameSide_PowerIsTwo(Side side, Column column)
+        {
+            var game = TestHelpers.PlayCards(
+                3,
+                side == Side.Top
+                    ? new[] { ("Star-Lord", column), ("Misty Knight", column) }
+                    : new (string, Column)[] { },
+                side == Side.Bottom
+                    ? new[] { ("Star-Lord", column), ("Misty Knight", column) }
+                    : new (string, Column)[] { }
+            );
+
+            Assert.That(game[column][side].Count == 2);
+
+            var rocketRaccoon = game[column][side][0];
+            Assert.That(rocketRaccoon.Name, Is.EqualTo("Star-Lord"));
+
+            Assert.That(rocketRaccoon.Power, Is.EqualTo(2));
+        }
+
+        [Test]
+        [TestCase(Side.Top, Column.Middle)]
+        [TestCase(Side.Bottom, Column.Middle)]
+        public void PlayCardWithAnotherCardOnOpposingSide_PowerIsFive(Side side, Column column)
+        {
+            var game = TestHelpers.PlayCards(
+                3,
+                side == Side.Top
+                    ? new[] { ("Star-Lord", column) }
+                    : new[] { ("Misty Knight", column) },
+                side == Side.Bottom
+                    ? new[] { ("Star-Lord", column) }
+                    : new[] { ("Misty Knight", column) }
+            );
+
+            Assert.That(game[column][side].Count == 1);
+
+            var rocketRaccoon = game[column][side][0];
+            Assert.That(rocketRaccoon.Name, Is.EqualTo("Star-Lord"));
+
+            Assert.That(rocketRaccoon.Power, Is.EqualTo(5));
+        }
+    }
+}
