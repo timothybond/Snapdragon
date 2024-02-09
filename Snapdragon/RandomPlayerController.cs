56e8a7a9704a843dd86a2e034a3c4e80dfe613a5
tim <9168829+timothybond@users.noreply.github.co>
2024-02-09 11:58:54 -0600

    Fixing bug where RandomPlayerController would never perform moves, which also by extension meant that the Monte Carlo controller couldn't understand the future benefits of them in some cases.

--- Snapdragon/RandomPlayerController.cs
+++ Snapdragon/RandomPlayerController.cs
@@ -1,28 +1,15 @@
-﻿using Snapdragon.PlayerActions;
-using static Snapdragon.ControllerUtilities;
+﻿using static Snapdragon.ControllerUtilities;
 
 namespace Snapdragon
 {
     /// <summary>
-    /// An implementation of <see cref="IPlayerController"/> that essentially just plays cards in random locations.
+    /// An implementation of <see cref="IPlayerController"/> that essentially just makes random moves.
     /// </summary>
     public class RandomPlayerController : IPlayerController
     {
         public IReadOnlyList<IPlayerAction> GetActions(Game game, Side side)
         {
-            // TODO: Also have Move actions
-            var playableCardSets = GetPlayableCardSets(game[side]);
-            var availableSlots = GetAvailableCardSlots(game, side);
-            var totalSlots = availableSlots.Left + availableSlots.Middle + availableSlots.Right;
-
-            // Ignore card sets with too many cards (note than an empty set is always present)
-            playableCardSets = playableCardSets.Where(s => s.Count <= totalSlots).ToList();
-
-            var cardsToPlay = Random.Of(playableCardSets);
-
-            var columns = GetRandomColumns(cardsToPlay.Count, availableSlots);
-
-            return cardsToPlay.Select((c, i) => new PlayCardAction(side, c, columns[i])).ToList();
+            return Random.Of(GetPossibleActionSets(game, side).ToList());
         }
     }
 }
