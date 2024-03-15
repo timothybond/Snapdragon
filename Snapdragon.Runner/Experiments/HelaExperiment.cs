7f8c5618ce1cc7b8f5d8d289bdd7b4b9bfea03be
tim <9168829+timothybond@users.noreply.github.co>
2024-03-15 11:47:01 -0500

    Experiment to see if Hela causes more Discard abilities to be present.

---
+++ Snapdragon.Runner/Experiments/HelaExperiment.cs
@@ -0,0 +1,33 @@
+﻿using Snapdragon.CardOrders;
+using Snapdragon.GeneticAlgorithm;
+
+namespace Snapdragon.Runner.Experiments
+{
+    public class HelaExperiment
+    {
+        public async Task Run(ISnapdragonRepositoryBuilder? repositoryBuilder = null)
+        {
+            const int Simulations = 20;
+            const int MutationsPer = 100;
+
+            var helaDefinition = SnapCards.ByName["Hela"];
+
+            var selfPlay = new PopulationSelfPlay(
+                Guid.NewGuid(),
+                "Hela Pinned Self Play 200x256",
+                DateTimeOffset.UtcNow,
+                repositoryBuilder
+            );
+
+            var helaPinned = new Genetics(
+                [helaDefinition],
+                SnapCards.All,
+                new MonteCarloSearchController(Simulations),
+                MutationsPer,
+                new RandomCardOrder()
+            );
+
+            await selfPlay.Run(helaPinned, "with-hela-solo", 256, 200, 10);
+        }
+    }
+}
